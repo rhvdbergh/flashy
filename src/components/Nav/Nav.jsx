7cd9d8ccc033fcfd49348b4ddd47d38c629b3fe5
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-14 17:13:10 -0600

    nav bar displays according to context and authentication

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,55 +1,114 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
+import { Link, useHistory } from 'react-router-dom';
 import './Nav.css';
-import { useSelector } from 'react-redux';
-import { Box, Typography, ButtonGroup, Button } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  Box,
+  Typography,
+  ButtonGroup,
+  Button,
+  AppBar,
+  Toolbar,
+} from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+// styling for mui
+const useStyles = makeStyles(() => ({
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+}));
 
 function Nav() {
   const user = useSelector((store) => store.user);
   const navTitle = useSelector((store) => store.navTitle);
 
+  // set up redux dispatch
+  const dispatch = useDispatch();
+
+  // set up the useHistory hook
+  const history = useHistory();
+
+  // set up the styling for mui
+  const { toolbar } = useStyles();
+
   return (
-    <Box className="nav">
-      <Link to="/home">
-        <Typography variant="h4" className="nav-title">
-          Flashy
-        </Typography>
-      </Link>
-      <Box>
-        {/* If no user is logged in, show these links */}
-        {user.id === null && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
+    <header className="nav">
+      <AppBar>
+        <Toolbar className={toolbar}>
+          <Link to="/home">
+            {/* Adding component="h1" here so screen readers pick up on it, it's the main heading */}
+            <Typography variant="h4" component="h1" className="nav-title">
+              Flashy
+            </Typography>
           </Link>
-        )}
+          <Box>
+            <Typography variant="h5" className="nav-title">
+              {navTitle}
+            </Typography>
+          </Box>
+          <Box>
+            {/* If no user is logged in, show these links */}
+            {user.id === undefined && (
+              // If there's no user, show login/registration links
+              <Button
+                key="login"
+                color="inherit"
+                className="navLink"
+                onClick={() => history.push('/login')}
+              >
+                Login / Register
+              </Button>
+            )}
 
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Box>
-              <Typography variant="h5" className="nav-title">
-                {navTitle}
-              </Typography>
-            </Box>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
+            {/* If a user is logged in, show these links */}
+            {user.id && (
+              <Button
+                key="Home"
+                color="inherit"
+                className="navLink"
+                onClick={() => history.push('/home')}
+              >
+                Home
+              </Button>
+            )}
 
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
+            <Button
+              className="navLink"
+              key="Back"
+              color="inherit"
+              onClick={() => history.goBack()}
+            >
+              Back
+            </Button>
 
-            <LogOutButton className="navLink" />
-          </>
-        )}
+            {/* For the moment, there is no about page  */}
+            {/* <Button
+              className="navLink"
+              color="inherit"
+              key="About"
+              onClick={() => history.push('/about')}
+            >
+              About
+            </Button> */}
 
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-      </Box>
-    </Box>
+            {/* If a user is logged in, show these links */}
+            {user.id && (
+              <Button
+                className="navLink"
+                key="Log Out"
+                color="inherit"
+                onClick={() => dispatch({ type: 'LOGOUT' })}
+              >
+                Log Out
+              </Button>
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </header>
   );
 }
 
--- src/redux/reducers/utils.reducer.js
+++ src/redux/reducers/utils.reducer.js
@@ -3,6 +3,12 @@ const navTitle = (state = '', action) => {
   switch (action.type) {
     case 'SET_NAV_TITLE':
       return action.payload;
+    // reset in case of user logout
+    case 'LOGOUT':
+      return '';
+    // in case a title is not necessary, or to ensure wrong title doesn't display
+    case 'RESET_NAV_TITLE':
+      return '';
     default:
       return state;
   }
