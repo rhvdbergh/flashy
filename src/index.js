79d0f339b7b5626375e421e174901c4715853c23
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-14 14:52:40 -0600

    mui setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+// mui fonts
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import store from './redux/store';
 
@@ -10,5 +15,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('react-root'),
+  document.getElementById('react-root')
 );
