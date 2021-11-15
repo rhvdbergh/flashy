235ab75c3ad3d7b054d9cbe29e098cb833069331
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-15 13:45:17 -0600

    only allows authenticated to GET /api/class route

---
+++ server/routes/class.router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+
+// GET /api/class
+// fetches all the classes belonging to a logged in teacher
+router.get('/', rejectUnauthenticated, (req, res) => {
+  console.log(`this is the user`, req.user);
+  // GET route code here
+  res.sendStatus(200);
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+  // POST route code here
+});
+
+module.exports = router;
