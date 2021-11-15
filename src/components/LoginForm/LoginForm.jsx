3aa21080cbcf71a8feba53518ed3b748491ec685
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-15 11:42:42 -0600

    decently styled login screen

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,12 +1,42 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+// import mui
+import { makeStyles } from '@mui/styles';
+import {
+  FormControl,
+  Typography,
+  Input,
+  Button,
+  TextField,
+} from '@mui/material';
+
+// styling for mui
+const useStyles = makeStyles(() => ({
+  form: {
+    marginTop: '100px',
+    marginBottom: '30px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '400px',
+    width: '300px',
+    textAlign: 'center',
+  },
+  input: {
+    margin: '20px',
+    width: '100%',
+  },
+}));
 
 function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const errors = useSelector(store => store.errors);
+  const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
+
+  // set up the mui style
+  const { form, input } = useStyles();
 
   const login = (event) => {
     event.preventDefault();
@@ -25,41 +55,47 @@ function LoginForm() {
   }; // end login
 
   return (
-    <form className="formPanel" onSubmit={login}>
-      <h2>Login</h2>
-      {errors.loginMessage && (
-        <h3 className="alert" role="alert">
-          {errors.loginMessage}
-        </h3>
-      )}
-      <div>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            required
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            required
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input className="btn" type="submit" name="submit" value="Log In" />
-      </div>
-    </form>
+    <FormControl>
+      <form className={form} onSubmit={login}>
+        <Typography variant="h3">Login</Typography>
+        {errors.loginMessage && (
+          <h3 className="alert" role="alert">
+            {errors.loginMessage}
+          </h3>
+        )}
+        <TextField
+          type="text"
+          label="Username"
+          size="small"
+          required
+          className={input}
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <TextField
+          type="password"
+          label="Password"
+          size="small"
+          required
+          className={input}
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <Button type="submit" name="submit" value="Log In" variant="contained">
+          Login
+        </Button>
+        <Typography variant="body1"> ... or click below to register</Typography>
+        <Button
+          variant="contained"
+          name="registration"
+          onClick={() => {
+            history.push('/registration');
+          }}
+        >
+          Register
+        </Button>
+      </form>
+    </FormControl>
   );
 }
 
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
+// import mui
+import { Box, Paper } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+// set up the mui styles
+const useStyles = makeStyles(() => ({
+  registrationBox: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}));
 
 function LoginPage() {
   const history = useHistory();
 
-  return (
-    <div>
-      <LoginForm />
+  // get the mui styles
+  const { registrationBox } = useStyles();
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
-      </center>
-    </div>
+  return (
+    <Box className={registrationBox}>
+      <LoginForm />
+    </Box>
   );
 }
 
