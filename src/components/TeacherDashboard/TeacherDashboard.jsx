9920f506c1a4a680a423f969684acedbe9c3c61d
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-15 13:38:09 -0600

    add class button navigates to edit class

--- src/components/TeacherDashboard/TeacherDashboard.jsx
+++ src/components/TeacherDashboard/TeacherDashboard.jsx
@@ -5,17 +5,45 @@
 
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+// import mui
+import { Box, Button, Container } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+// set up the mui styles
+const useStyles = makeStyles(() => ({
+  container: {
+    marginTop: '100px',
+  },
+}));
 
 function TeacherDashboard() {
   // set up the redux dispatch
   const dispatch = useDispatch();
+
+  // set up the useHistory hook
+  const history = useHistory();
+
+  // get the mui styles
+  const { container } = useStyles();
 
   // on page load, set nav bar title
   useEffect(() => {
     dispatch({ type: 'SET_NAV_TITLE', payload: 'Dashboard' });
   }, []);
 
-  return <p>TeacherDashboard Component</p>;
+  return (
+    <Container className={container}>
+      <Button
+        variant="contained"
+        value="add_class"
+        onClick={() => history.push('/editclass')}
+      >
+        Add Class
+      </Button>
+    </Container>
+  );
 }
 
 export default TeacherDashboard;
