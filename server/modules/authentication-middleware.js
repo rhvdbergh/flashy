6d03b5c54635818231a3f016d019339212c34366
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-15 13:51:10 -0600

    adds authorization middleware for teachers and students

--- server/modules/authentication-middleware.js
+++ server/modules/authentication-middleware.js
@@ -10,4 +10,24 @@ const rejectUnauthenticated = (req, res, next) => {
   }
 };
 
-module.exports = { rejectUnauthenticated };
+// only allow this if the user has a role of teacher
+const onlyAllowTeacher = (req, res, next) => {
+  if (req.user.role === 'teacher') {
+    // they're a teacher
+    next();
+  } else {
+    res.sendStatus(403); // tell them it's forbidden!
+  }
+};
+
+// only allow this if the user has a role of student
+const onlyAllowStudent = (req, res, next) => {
+  if (req.user.role === 'student') {
+    // they're a teacher
+    next();
+  } else {
+    res.sendStatus(403); // tell them it's forbidden!
+  }
+};
+
+module.exports = { rejectUnauthenticated, onlyAllowTeacher, onlyAllowStudent };
--- server/routes/class.router.js
+++ server/routes/class.router.js
@@ -3,11 +3,12 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const {
   rejectUnauthenticated,
+  onlyAllowTeacher,
 } = require('../modules/authentication-middleware');
 
 // GET /api/class
 // fetches all the classes belonging to a logged in teacher
-router.get('/', rejectUnauthenticated, (req, res) => {
+router.get('/', rejectUnauthenticated, onlyAllowTeacher, (req, res) => {
   console.log(`this is the user`, req.user);
   // GET route code here
   res.sendStatus(200);
