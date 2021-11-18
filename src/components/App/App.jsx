69663da98e67b9805c2e787e7323ee98595349ee
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-18 09:44:50 -0600

    removes footer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,7 +9,6 @@ import {
 import { useDispatch, useSelector } from 'react-redux';
 
 import Nav from '../Nav/Nav';
-import Footer from '../Footer/Footer';
 
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
@@ -140,7 +139,6 @@ function App() {
             <h1>404</h1>
           </Route>
         </Switch>
-        <Footer />
       </div>
     </Router>
   );
