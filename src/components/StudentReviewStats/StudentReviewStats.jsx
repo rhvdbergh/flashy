23304dd3a8c5e21acc310ac51ac893ecf02ba831
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-18 22:37:34 -0600

    shows table, buttons without data

--- src/components/StudentReviewStats/StudentReviewStats.jsx
+++ src/components/StudentReviewStats/StudentReviewStats.jsx
@@ -5,9 +5,45 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
+// import mui
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Select,
+  InputLabel,
+  MenuItem,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+  Paper,
+  Badge,
+} from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { useHistory } from 'react-router-dom';
+
+// set up the mui styles
+const useStyles = makeStyles(() => ({
+  container: {
+    marginTop: '100px',
+    width: '100%',
+  },
+}));
+
 function StudentReviewStats() {
   // set up the redux dispatch
   const dispatch = useDispatch();
+
+  // set up the useHistory hook to navigate
+  const history = useHistory();
+
+  // get the mui styles
+  const { container } = useStyles();
 
   // on page load, set nav bar title
   useEffect(() => {
@@ -15,7 +51,32 @@ function StudentReviewStats() {
     // ensure that the back button is displayd on this page
     dispatch({ type: 'SET_DISPLAY_BACK_BUTTON', payload: true });
   }, []);
-  return <p>StudentReviewStats Component</p>;
+  return (
+    <Container className={container}>
+      <Box>
+        <Typography variant="h2">Good job!</Typography>
+        <Typography variant="h5">Today's Stats</Typography>
+      </Box>
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell>New cards learned:</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Cards to review: </TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+          <TableRow></TableRow>
+          <TableCell>Cards already learned:</TableCell>
+          <TableCell></TableCell>
+        </TableBody>
+      </Table>
+      <Button variant="contained" onClick={() => history.push(`/`)}>
+        Continue
+      </Button>
+    </Container>
+  );
 }
 
 export default StudentReviewStats;
