3ba81058d1342bb111a33a18605e87f27f6725b3
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-24 15:24:59 -0600

    fixes card assigned to stack assigned in class table on teacher dashboard

--- src/components/TeacherDashboard/TeacherDashboard.jsx
+++ src/components/TeacherDashboard/TeacherDashboard.jsx
@@ -83,7 +83,7 @@ function TeacherDashboard() {
               <TableRow>
                 <TableCell>Class Name</TableCell>
                 <TableCell align="center">Delete</TableCell>
-                <TableCell align="center">Card Assigned</TableCell>
+                <TableCell align="center">Stack Assigned</TableCell>
                 <TableCell align="center">View Progress</TableCell>
                 <TableCell align="center">Settings</TableCell>
               </TableRow>
