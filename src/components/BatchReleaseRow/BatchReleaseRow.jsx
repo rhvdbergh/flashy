63bae4cdd5fb2f0c2df5bf70844b6e55450834ae
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-11-27 09:40:18 -0600

    date pickers display correct time

--- src/components/BatchReleaseRow/BatchReleaseRow.jsx
+++ src/components/BatchReleaseRow/BatchReleaseRow.jsx
@@ -1,7 +1,33 @@
 //import mui
-import { Typography } from '@mui/material';
+import { Typography, TextField } from '@mui/material';
+import DateAdapter from '@mui/lab/AdapterLuxon';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import { useState } from 'react';
+
 function BatchReleaseRow({ batch }) {
-  return <Typography variant="body1">Batch {batch}</Typography>;
+  // set local state for the date
+  const [date, setDate] = useState(new Date());
+
+  const handleChange = (newDate) => {
+    console.log('things changed', newDate);
+    setDate(newDate);
+  };
+
+  return (
+    <>
+      <LocalizationProvider dateAdapter={DateAdapter}>
+        <DesktopDatePicker
+          label="Date desktop"
+          inputFormat="MM/dd/yyyy"
+          value={date}
+          onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+      <Typography variant="body1">Batch {batch}</Typography>{' '}
+    </>
+  );
 }
 
 export default BatchReleaseRow;
